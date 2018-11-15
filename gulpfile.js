ee35328ae989b93ab4a0308ab7607a6880bd683e
timoffmax <tim.off.max@gmail.com>
2018-11-15 14:17:13 +0200

    Added fonts and color variables

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task('sass', function() {
 	.pipe(sass({outputStyle: 'expanded'}).on("error", notify.onError()))
 	.pipe(rename({suffix: '.min', prefix : ''}))
 	.pipe(autoprefixer(['last 15 versions']))
-	.pipe(cleanCSS()) // Опционально, закомментировать при отладке
+	.pipe(cleanCSS())
 	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.stream())
 });
